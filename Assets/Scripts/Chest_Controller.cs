ed63e7d40e9f200efedb82d99987b79c5aae7e86
ionthas <ionthasMail@gmail.com>
2015-07-16 01:01:55 +0200

    Mask generation added
    - Added helper function CreateBlendMesh for the creation of the meshes of the chests.
    - Added the MaskGenerator script to test how to use BlendShapes more eficciently and to have some items to put inside the chests.
    - Variation of the CreateBlendMesh called CreateMask. We need to optimize that function.

--- Assets/Scripts/Chest_Controller.cs
+++ Assets/Scripts/Chest_Controller.cs
@@ -2,9 +2,7 @@
 
 public class Chest_Controller : MonoBehaviour {
 
-    //private Animator animator;
     private bool open = false;
-    //private int openHash = Animator.StringToHash("Open");
 
     private AudioSource audioSource;
     public AudioClip chestOpen;
@@ -18,27 +16,18 @@ public class Chest_Controller : MonoBehaviour {
     private int blendShapeCountBottom = 0;
     private int blendShapeCountTop = 0;
 
-    private MeshCollider meshColliderBottom;
-    private MeshCollider meshColliderTop;
-
     [SerializeField][Range(0, 1)] float size;
     [SerializeField][Range(0, 1)] float depth;
 
     void Awake()
     {
-        //animator = GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();
 
-        //meshColliderBottom = transform.FindChild("Bottom").GetComponent<MeshCollider>();
-        //meshColliderTop = transform.FindChild("Top").GetComponent<MeshCollider>();
-
-        // Using BlendShapes
-
-        // Bottom
+        // Getting Renderer and Mesh for the bottom part of the chest.
         skinnedMeshRendererBottom = transform.FindChild("Bottom").GetComponent<SkinnedMeshRenderer>();
         skinnedMeshBottom = skinnedMeshRendererBottom.sharedMesh;
 
-        // Top
+        // Getting Renderer and Mesh for the top part of the chest.
         skinnedMeshRendererTop = transform.FindChild("Top").GetComponent<SkinnedMeshRenderer>();
         skinnedMeshTop = skinnedMeshRendererTop.sharedMesh;
     }
@@ -46,65 +35,25 @@ public class Chest_Controller : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
-        // Detecting how many BlendShapes we have.
-        blendShapeCountBottom = skinnedMeshBottom.blendShapeCount;
-        Debug.Log("BlendShape count bottom: " + blendShapeCountBottom);
-
-        // Applying BlendShapes.
-        if (blendShapeCountBottom != 0)
-            skinnedMeshRendererBottom.SetBlendShapeWeight(0, size * 100);
-        
-        // Creates a snapshot of the SkinnedMeshRenderer and stores it in the mesh.
-        // That skinned mesh renderer should have the shape with the BlendShapes applyied.
-        Mesh bakedMesh = new Mesh();
-        skinnedMeshRendererBottom.BakeMesh(bakedMesh);
-
-        // Recalcultate the bounding volume of the mesh from the vertices.
-        bakedMesh.RecalculateBounds();
-        Debug.Log("Baked mesh bounds: " + bakedMesh.bounds.ToString());
-
-
-        GameObject child = transform.FindChild("Bottom").gameObject;
-        DestroyImmediate(child.GetComponent<MeshCollider>());
-
-        meshColliderBottom = child.AddComponent<MeshCollider>();
-        meshColliderBottom.sharedMesh = bakedMesh;
-        meshColliderBottom.convex = false;
-
+        CreateBlendMesh(skinnedMeshRendererBottom, skinnedMeshBottom, "Bottom", false);
+        CreateBlendMesh(skinnedMeshRendererTop, skinnedMeshTop, "Top", true);
     }
 
-	// TODO: It's more eficcient to compare hash id's instead of strings.
-	void Update () {
-
-
-        //DestroyImmediate(transform.FindChild("Bottom").GetComponent<MeshCollider>());
-        //meshColliderBottom = 
-
-        //DestroyImmediate(this.GetComponent<MeshCollider>());
-        //var collider = this.AddComponent<MeshCollider>();
-        //collider.sharedMesh = myMesh;
-
-        //meshColliderBottom.sharedMesh = null;
-        //meshColliderBottom.sharedMesh = skinnedMeshBottom;
-
-        //meshColliderBottom.transform. = new Vector3(2, 1, 1);
-
-        //skinnedMeshRendererBottom.
-        //meshCollider.sharedMesh = skinnedMeshRendererBottom.sharedMesh;
-
-        // The index of the layer we are dealing (base layer = 0).
-        //AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
-
+    void Update()
+    {
         // TODO: Possible to have the sound clip depending on the rotation of the pivot of the chest?
-        // TODO: Implement opening from the front of the chest only.
         if (Input.GetMouseButtonDown(0))
         {
+            Rigidbody rb = transform.FindChild("Top").GetComponent<Rigidbody>();
+            rb.AddForce(transform.up * 100);
+
             // XOR the open boolean.
             open = !open;
 
-            // Setting the open boolean for the animator to the current open value.
-            //animator.SetBool(openHash, open);
-            
+            HingeJoint hingeJoint = transform.FindChild("Bottom").GetComponent<HingeJoint>();
+            Debug.Log(hingeJoint.angle);
+
+
             // Assigning the correct clip.
             audioSource.clip = (open) ? chestOpen : chestClose;
 
@@ -114,6 +63,37 @@ public class Chest_Controller : MonoBehaviour {
 
         }
 
+    }
+
+    // TODO: Put this into Misc class?
+    private void CreateBlendMesh(SkinnedMeshRenderer skinnedMeshRenderer, Mesh skinnedMesh, string name, bool convex)
+    {
+        // Detecting how many BlendShapes we have.
+        int blendShapeCount = 0;
+        blendShapeCount = skinnedMesh.blendShapeCount;
+        Debug.Log("BlendShape count bottom: " + blendShapeCount);
+
+        // Applying BlendShapes.
+        if (blendShapeCount != 0)
+            skinnedMeshRenderer.SetBlendShapeWeight(0, size * 100);
+
+        // Creates a snapshot of the SkinnedMeshRenderer and stores it in the mesh.
+        // That skinned mesh renderer should have the shape with the BlendShapes applyied.
+        Mesh bakedMesh = new Mesh();
+        skinnedMeshRenderer.BakeMesh(bakedMesh);
+
+        // Recalcultate the bounding volume of the mesh from the vertices.
+        bakedMesh.RecalculateBounds();
+        Debug.Log("Baked mesh bounds: " + bakedMesh.bounds.ToString());
+    
+        // Selecting part and destroying MeshCollider in case there is one.
+        GameObject child = transform.FindChild(name).gameObject;
+        DestroyImmediate(child.GetComponent<MeshCollider>());
+
+        // Adding MeshCollider and assigning the bakedMesh.
+        MeshCollider meshCollider = child.AddComponent<MeshCollider>();
+        meshCollider.sharedMesh = bakedMesh;
+        meshCollider.convex = convex;
     }
 
 }
---
+++ Assets/Scripts/MaskGenerator.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System;
+
+public class MaskGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject maskPrefab;
+    [SerializeField] int maskNumber = 10;
+    [SerializeField] string seed = "lolerpoper";
+    [SerializeField] bool useRandomSeed = true;
+
+    private System.Random pseudoRNG;
+
+    SkinnedMeshRenderer skinnedMeshRenderer;
+    Mesh skinnedMesh;
+
+    Texture[] textureList;
+
+    int spacing = 2;
+
+    void Start ()
+    {
+        // Loading all mask textures.
+        string localPath = "Zones/Ruins/Items/Mask/Textures";
+        textureList = Resources.LoadAll<Texture>(localPath);
+     
+
+        if (useRandomSeed)
+            seed = DateTime.Now.Ticks.ToString();
+
+        // Getting random seed.
+        pseudoRNG = new System.Random(seed.GetHashCode());
+
+        // TODO: Get random texture and apply it to mesh.
+        for (int x = 0; x < maskNumber; x++)
+        {
+            for (int y = 0; y < maskNumber; y++)
+            {
+                GameObject mask = CreateMask(maskPrefab, true);
+                mask.transform.position = new Vector3(x * spacing, y * spacing, 0);
+                //Debug.Log(mask.transform.rotation);
+                //mask.transform.rotation = new Quaternion(-0.7f, -0.7f, 0, 0.7f);
+            }
+        }    
+
+    }
+
+    private GameObject CreateMask(GameObject mask, bool convex)
+    {
+        // Getting the SkinnedMeshRenderer and the mesh of the prefab.
+        skinnedMeshRenderer = mask.GetComponent<SkinnedMeshRenderer>();
+        skinnedMesh = skinnedMeshRenderer.sharedMesh;
+
+        // Applying BlendShapes with random values.
+        for (int i = 0; i < skinnedMesh.blendShapeCount; i++)
+        {
+            skinnedMeshRenderer.SetBlendShapeWeight(i, pseudoRNG.Next(0, 100));
+        }
+
+        // Creates a snapshot of the SkinnedMeshRenderer and stores it in the mesh.
+        // That skinned mesh renderer should have the shape with the BlendShapes applyied.
+        Mesh bakedMesh = new Mesh();
+        skinnedMeshRenderer.BakeMesh(bakedMesh);
+
+        // Recalcultate the bounding volume of the mesh from the vertices.
+        bakedMesh.RecalculateBounds();
+
+        // Selecting part and destroying MeshCollider in case there is one.
+        DestroyImmediate(mask.GetComponent<MeshCollider>());
+
+        // Adding MeshCollider and assigning the bakedMesh.
+        MeshCollider meshCollider = mask.AddComponent<MeshCollider>();
+        meshCollider.sharedMesh = bakedMesh;
+        meshCollider.convex = convex;
+
+        // Selecting random texture.
+        Texture texture = textureList[pseudoRNG.Next(0, textureList.Length - 1)];
+
+        // Generating material with randomly chosen texture.
+        Material material = new Material(Shader.Find("Standard"));
+        material.SetTexture("_MainTex", texture);
+        skinnedMeshRenderer.material = material;
+
+        return Instantiate(mask);
+    }
+
+}
